72cce92cef5ddf462816158a4c68e071f770970b
Mathias Sulser <suls@suls.org>
2008-09-13 22:09:12 +0200

    initializes new config

--- spec/smsr/config_spec.rb
+++ spec/smsr/config_spec.rb
@@ -1,10 +1,25 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe SmsR::Config, "fresh" do
+
+  before(:each) do
+    @mock_config_file = mock(File)    
+    
+  end
   
   it "should create new file while saving"
 
-  it "should initialize new config while loading"
+  it "should initialize new config while loading" do
+    File.stub!(:exists?).and_return(false)
+    SmsR::Config.should_receive(:new).
+                with(@mock_config_file).
+                and_return(42)
+    
+    c = SmsR::Config.load @mock_config_file
+    
+    c.should equal(42)
+    
+  end
 end
 
 describe SmsR::Config, "existing" do
