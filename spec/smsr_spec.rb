03c320a6ce56620b3a3bbd738ea01f8bd22b5176
Mathias Sulser <suls@suls.org>
2008-09-13 19:25:53 +0200

    option parser spec defined and version spec added

--- spec/smsr_spec.rb
+++ spec/smsr_spec.rb
@@ -1,7 +1,27 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe SmsR, "option parser" do
-  it "should enable debugging log by adding -d cmd option"
+  it "should enable debugging log by adding --debug cmd option" do
+    SmsR.stub!(:start)
+    
+    SmsR.should be_debug
+  end
   
-  it "should disable the debug log by default"   
+  it "should disable the debug log by default" do
+    SmsR.stub!(:start)
+    
+    SmsR.should_not be_debug
+  end
+end
+
+describe SmsR::VERSION do
+  it "should print the version string as MAYOR.MINOR.TINY" do
+    v = "#{SmsR::VERSION::MAJOR}." +
+        "#{SmsR::VERSION::MINOR}." +
+        "#{SmsR::VERSION::TINY}"
+        
+    SmsR::VERSION::STRING.should eql(v)
+  end
+  
+  it "should be the same as in the .gemspec" 
 end
