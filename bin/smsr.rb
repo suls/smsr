95cc8a7230e982688f3b1a0bd6d4a5b4b5b308f4
Mathias Sulser <suls@suls.org>
2008-09-13 17:44:40 +0200

    let's start with the debug log ;)

--- bin/smsr.rb
+++ bin/smsr.rb
@@ -1,1 +1,4 @@
 #!/usr/bin/env ruby -wKU
+require File.dirname(__FILE__) + '/../lib/smsr'
+ 
+SmsR.start ARGV
--- lib/smsr.rb
+++ lib/smsr.rb
@@ -2,4 +2,24 @@ $:.unshift File.dirname(__FILE__)
 
 module SmsR
   VERSION = '0.1.0'
+  
+  # http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/232636
+  extend self
+  
+  def start(args)
+    # @options = parse_options(args)
+    @options = {}
+    @debug = @options[:debug] = true
+    debug "SmsR #{VERSION} started .."
+  end
+  
+  def debug(*messages)
+    puts messages.map { |m| "** #{m}" } if debug?
+  end
+
+  def debug?
+    !!@debug
+  end
+  
+  
 end
