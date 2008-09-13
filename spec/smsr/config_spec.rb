51bd298fbdb43737f29abba9946fcfb2064a96a8
Mathias Sulser <suls@suls.org>
2008-09-13 22:23:56 +0200

    exchanged mock with file

--- spec/smsr/config_spec.rb
+++ spec/smsr/config_spec.rb
@@ -3,22 +3,17 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe SmsR::Config, "fresh" do
 
   before(:each) do
-    @mock_config_file = mock(File)    
+    @test_file = "/tmp/suhv_test"
     
+    File.delete(@test_file) if File.exists? @test_file
   end
   
   it "should create new file while saving"
 
   it "should initialize new config while loading" do
-    File.stub!(:exists?).and_return(false)
-    SmsR::Config.should_receive(:new).
-                with(@mock_config_file).
-                and_return(42)
+    c = SmsR::Config.load @test_file
     
-    c = SmsR::Config.load @mock_config_file
-    
-    c.should equal(42)
-    
+    c.should be_instance_of(SmsR::Config)
   end
 end
 
