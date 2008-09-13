64d5663e3ba1c96f344038a67d508bce890e6109
Mathias Sulser <suls@suls.org>
2008-09-13 17:44:06 +0200

    specs added

---
+++ spec/smsr_spec.rb
@@ -0,0 +1,7 @@
+require File.dirname(__FILE__) + '/spec_helper'
+
+describe SmsR, "option parser" do
+  it "should enable debugging log by adding -d cmd option"
+  
+  it "should disable the debug log by default"   
+end
