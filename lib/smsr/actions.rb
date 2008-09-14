7b5977f704421bab087ea71c62b59d07ea8b8721
Mathias Sulser <suls@suls.org>
2008-09-15 01:29:27 +0200

    basic config action working, info log added

--- lib/smsr/actions.rb
+++ lib/smsr/actions.rb
@@ -32,11 +32,13 @@ module Actions
       SmsR.debug "Store entry for #{provider}"
       SmsR.config[:"#{provider}"] = user, password
       SmsR.config.save!
+      SmsR.info "#{provider} added to config"
     end
 
     runnable do |provider|
-      SmsR.debug "Show entry for #{provider}"
-      puts SmsR.config[:"#{provider}"]
+      p_c = SmsR.config[:"#{provider}"]
+      SmsR.info "#{provider} :"
+      %w{user password}.each { |e|  SmsR.info "  #{e}: #{p_c.send e}"}
     end    
     
   end
