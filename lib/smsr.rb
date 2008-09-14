f42bf90828eb7f8ba9e3be8d9a9bd2656e4c5d8e
Mathias Sulser <suls@suls.org>
2008-09-14 03:12:50 +0200

    actions introduced.

--- lib/smsr.rb
+++ lib/smsr.rb
@@ -2,20 +2,36 @@ $:.unshift File.dirname(__FILE__)
 
 require "smsr/version"
 require "smsr/config"
+require "smsr/actions"
 require "ostruct"
 require "optparse"
 
 module SmsR
-
+  
+  alias original_send send
+  include Actions
+  
   # http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/232636
   extend self
   
   def start(args, io=STDOUT)
     @io = io
+    args_copy = args.dup
     @options = parse_options(args)
     @debug = @options.debug
+    
+    debug "args: ", args_copy << " "
+    debug "options: ", options
+    
     debug "SmsR #{VERSION::STRING} started .."
-    debug *args
+    
+    if args
+      debug "available actions: ", *Actions.instance_methods << " "
+      action = args.shift
+      debug "selected action: #{action}"
+      original_send(action) if Actions.instance_methods.include?(action)
+    end
+    debug ".. exiting SmsR"
   end
   
   def debug(*messages)
---
+++ lib/smsr/actions.rb
@@ -0,0 +1,9 @@
+module Actions
+  def config
+    debug "in config"
+  end
+  
+  def send
+    debug "in send"
+  end
+end
