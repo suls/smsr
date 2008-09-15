96d3287d2a1c4ec23b3a69bcb4789759d7951b22
Mathias Sulser <suls@suls.org>
2008-09-15 20:23:03 +0200

    default functionality

--- lib/smsr/actions/config.rb
+++ lib/smsr/actions/config.rb
@@ -6,12 +6,16 @@ module Actions
       SmsR.debug "Store entry for #{provider}"
       SmsR.config[:"#{provider}"] = user, password
       SmsR.config.save!
-      SmsR.info "#{provider} added to config"
+      SmsR.info "Config for #{provider} added."
     end
 
     runnable do |provider|
+      unless provider_itself[:exists?]
+        SmsR.info provider_itself[:error]
+        return
+      end
       p_c = SmsR.config[:"#{provider}"]
-      SmsR.info "#{provider} :"
+      SmsR.info "Saved config for Provider '#{provider}':"
       %w{user password}.each { |e|  SmsR.info "  #{e}: #{p_c.send e}"}
     end    
   
--- lib/smsr/actions/send.rb
+++ lib/smsr/actions/send.rb
@@ -3,11 +3,32 @@ module Actions
   
     class Send < RunnableAction
     runnable do 
-      SmsR.info "TODO: list available providers"
+      SmsR::Providers.load
+      
+      SmsR.info "Available providers:"
+      SmsR::Providers.providers.each { |k,v| SmsR.info "  #{k}"}
     end
     
-    runnable do |provider, number, message|
+    runnable do |provider_name, number, message|
+      SmsR::Providers.load
+      
+      unless provider_itself[:exists?]
+        SmsR.info provider_itself[:error]
+        return
+      end
+
+      unless provider_config[:exists?]
+        SmsR.info(*provider_config[:error])
+        return
+      end
+      
+      SmsR.debug "using #{provider} with config #{provider_config}"
+      
+      
+      provider.call number, message
       SmsR.info "TODO: implement sending"
+      
+      
     end
   end
 
