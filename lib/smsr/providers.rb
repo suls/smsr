05a927821ad715ace4fac9292fc346174cfdce51
Mathias Sulser <suls@suls.org>
2008-09-15 20:21:25 +0200

    added default load file

--- lib/smsr/providers.rb
+++ lib/smsr/providers.rb
@@ -1,5 +1,9 @@
 module SmsR  
 module Providers
+  
+  DEFAULT_PROVIDERS_FILE = File.dirname(__FILE__) + 
+                          '/../providers/default_providers'
+  
   extend self
   
   def provider(provider_name, &block)
@@ -11,7 +15,7 @@ module Providers
     @providers ||= {}
   end
   
-  def load(file, drop_existing=false)
+  def load(file=DEFAULT_PROVIDERS_FILE, drop_existing=false)
     if File.exists? file
       SmsR.debug "Loading providers from: #{file}"
       data = File.read(file)
@@ -20,6 +24,7 @@ module Providers
     else
       SmsR.debug "Provider file #{file} doesn't exist."
     end
+    @providers
   end
   
   class Provider
