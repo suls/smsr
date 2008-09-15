502cf3a1d91faccb67d13d8685d4158b41cb418f
Mathias Sulser <suls@suls.org>
2008-09-15 17:45:38 +0200

    loads provider from a file

--- lib/smsr/providers.rb
+++ lib/smsr/providers.rb
@@ -11,6 +11,17 @@ module Providers
     @providers ||= {}
   end
   
+  def load(file, drop_existing=false)
+    if File.exists? file
+      SmsR.debug "Loading providers from: #{file}"
+      data = File.read(file)
+      @providers = {} if drop_existing
+      module_eval data, file
+    else
+      SmsR.debug "Provider file #{file} doesn't exist."
+    end
+  end
+  
   class Provider
     def initialize(block)
       add_instance_method :call, block
--- spec/smsr/providers_spec.rb
+++ spec/smsr/providers_spec.rb
@@ -1,5 +1,7 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
+require 'fileutils'
+
 describe SmsR::Providers do
   before(:each) do
     SmsR::Providers.provider :test_provider do
@@ -23,5 +25,59 @@ describe SmsR::Providers do
     
     ret_val = SmsR::Providers.providers[:test_provider].call
     ret_val.should be(42)
+  end
+  
+  it "should generate Provider#call(N) when passed N arguments" do
+    SmsR::Providers.provider :test_provider_with_args do |a,b|
+      x = a*b
+    end
+    
+    ret_val = SmsR::Providers.providers[:test_provider_with_args].call(42, 42)
+    ret_val.should be(42*42)
+  end
+end
+
+describe SmsR::Providers, "from file" do
+  before(:each) do
+    @provider_file = "/tmp/smsr_provider_test"
+    # FileUtils.touch @provider_file
+    FileUtils.rm @provider_file, :force => true
+    provider_def = <<PD
+    provider :test_provider_from_file do
+      
+    end
+    
+PD
+    File.open(@provider_file, 'w') {|f| f.write(provider_def) }
+  end
+  
+  after(:each) do
+    FileUtils.rm @provider_file, :force => true
+  end
+  
+  it "should load the provider specified in a file" do
+    SmsR::Providers.load @provider_file, true
+    
+    SmsR::Providers.providers.should have(1).provider
+    
+    SmsR::Providers.providers[:test_provider_from_file].
+      should be_instance_of(SmsR::Providers::Provider)
+  end
+  
+  it "should load all the providers" do
+    provider_name = :test_provider_2_from_file
+    provider_def = <<PD
+    provider :#{provider_name} do
+      
+    end
+    
+PD
+    File.open(@provider_file, 'a') {|f| f.write(provider_def) }
+    SmsR::Providers.load @provider_file, true
+    
+    SmsR::Providers.providers.should have(2).provider
+    
+    SmsR::Providers.providers[provider_name].
+      should be_instance_of(SmsR::Providers::Provider)
   end
 end
