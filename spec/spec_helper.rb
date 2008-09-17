40271259ed0f1b0ec0d971ceb0f6cee712562592
Mathias Sulser <suls@suls.org>
2008-09-17 17:15:58 +0200

    mocking mixins with mixinmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,18 @@
 require File.dirname(__FILE__) + '/../lib/smsr'
+
+class MixinMock < Module
+  
+  def initialize(mock)
+    @@mock = mock
+  end
+  
+  def included(receiver)
+    receiver.class_eval do
+      def method_missing(meth, *args, &blk)
+        @@mock.send meth, args, blk
+      end
+    end
+    super
+  end
+  
+end
