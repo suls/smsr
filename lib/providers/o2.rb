7d94b8b3b9742d439627ab06f39cebca97c142d3
Mathias Sulser <suls@suls.org>
2008-11-03 23:15:20 +0000

    clicking second last button instead of submitting it

--- lib/providers/o2.rb
+++ lib/providers/o2.rb
@@ -3,5 +3,26 @@ require "mechanize"
 
 provider :o2 do |user, password, number, message|
   SmsR.debug "args: ", user, password, number, message
-
+  WWW::Mechanize.new do |agent|
+    SmsR.debug "logging in .."
+    site = "http://www.o2.co.uk/login?dest=http://sendtxt.o2.co.uk/sendtxt/action/compose"
+    agent.get(site) do |page|
+      form = page.form("o2portal_sign_in_form")
+      form["USERNAME"] = user
+      form["PASSWORD"] = password
+      form.submit
+    end
+    SmsR.debug ".. done"
+    
+    agent.get("http://sendtxt.o2.co.uk/sendtxt/action/compose") do |page|
+      
+      form =  page.form("sendtxtform")
+      form["compose.to"] = number
+      form["compose.message"] = message
+      # pp form.buttons[-2]
+      form.click_button form.buttons[-2]
+      SmsR.debug "start sending .."
+    end
+    SmsR.debug ".. done"
+  end
 end
